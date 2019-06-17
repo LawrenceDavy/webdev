c12f3515fb4df87641ec6f4c3054b523fdc3d942
LawrenceDavy13 <LawrenceDavy13@gmail.com>
2019-06-17 20:24:29 +0100

    update webpack config

---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+    entry: {
+        app: './src/index.js'
+    },
+    optimization: {
+        splitChunks: {
+            cacheGroups: {
+                styles: {
+                    name: 'styles',
+                    test: /\.css$/,
+                    chunks: 'all',
+                    enforce: true
+                }
+            }
+        }
+    },
+    devServer: {
+        hot: true,
+        compress: true,
+        contentBase: path.join(__dirname, 'dist'),
+        open: 'Chrome'
+    },
+    watch: true,
+    devtool: 'source-map',
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new MiniCssExtractPlugin({
+                filename: "style.css",
+                chunkFilename: "[name].css"
+        }),
+        new webpack.HotModuleReplacementPlugin()
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: "css-loader"
+                    },
+                    "sass-loader"
+                ]
+            }
+        ]
+    }
+}
